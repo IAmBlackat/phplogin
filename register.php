47a3ef00ee8649e9ecab2965866d77f37f378812
Saurabh G <34957562+IAmBlackat@users.noreply.github.com>
2024-02-22 16:50:20 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div id="form">
+        <h1>Registration Form</h1>
+        <form action="register_submit.php" method="post">
+            <label>Username:</label>
+            <input type="text" id="user" name="user"><br><br>
+            <label>Password:</label>
+            <input type="password" id="pass" name="pass"><br><br>
+            <input type="submit" id="btn" value="Register">
+        </form>
+    </div>
+</body>
+</html>
